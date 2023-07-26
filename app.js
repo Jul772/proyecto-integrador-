87ba49c5b9834aa0391adcc8a08014a776fc2272
Samux33 <samuelagustin332008@gmail.com>
2023-07-26 20:31:39 -0300

    no se pq no abre el server :(

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require("express")
 const path = require("path")
 const app = express()
 
+app.use(express.static('public'))
+
 app.listen(5000,() => (console.log("servidor abierto")))
+
 app.get("/",(req,res) => {
-    res.sendFile(path.join(__dirname,"/views/home.html"))
+    res.sendFile(path.resolve(__dirname,"./views/home.html"))
 })
