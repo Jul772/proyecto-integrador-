2d1ffbe3f7bddde6d5a364f71e67ef5b46379032
Samux33 <samuelagustin332008@gmail.com>
2023-07-26 20:37:23 -0300

    si funciona

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ app.use(express.static('public'))
 
 app.listen(5000,() => (console.log("servidor abierto")))
 
-app.get("/",(req,res) => {
+app.get('/',(req,res)=>{
     res.sendFile(path.resolve(__dirname,"./views/home.html"))
 })
