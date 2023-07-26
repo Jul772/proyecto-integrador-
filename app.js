20503a9306ec987ae4ac12ab2876a986e6f6b498
Samux33 <samuelagustin332008@gmail.com>
2023-07-26 20:25:27 -0300

    nodemon anadido

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ const app = express()
 
 app.listen(5000,() => (console.log("servidor abierto")))
 app.get("/",(req,res) => {
-    res.sendFile(path.join(__dirname,"/views/proyecto.html"))
+    res.sendFile(path.join(__dirname,"/views/home.html"))
 })
