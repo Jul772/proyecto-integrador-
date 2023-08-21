1c21623098d61b29f246400656efe01b8ba8f2e9
jul772 <juenares13@gmail.com>
2023-08-21 11:57:11 -0300

    aa

--- app.js
+++ app.js
@@ -7,10 +7,4 @@ app.listen(5000,() => (console.log("servidor abierto")))
 
 app.set('view engine','ejs')
 
-<<<<<<< HEAD
-app.use('/',homeRutas)
-
-app.listen(5000,() => (console.log("servidor abierto")))
-=======
 app.use('/',mainRutas)
->>>>>>> ee9e084467f11152c3a4da759f05af71aed94563
