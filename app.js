9688221ac9503346ffbef69a66f6d6fcff2a9a21
Samux33 <samuelagustin332008@gmail.com>
2023-08-30 19:38:28 -0300

    avance en home

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const mainRutas = require('./routes/main.js')
 app.use(express.static('public'))
 
-app.listen(5000,() => (console.log("servidor abierto")))
+app.listen(5000,() => (console.log("servidor abierto en el puerto 5000")))
 
 app.set('view engine','ejs')
 
--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,8 +1,12 @@
 const express=require('express')
+const path=require('path')
+const fs = require('fs');
+const productsFilePath = path.join(__dirname, '../data/productos.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 const homeController = {
     home: function(req,res){
-        res.render('home')
+        res.render('home',{products:products})
     },
     login: (req,res) => {
         res.render("login")
