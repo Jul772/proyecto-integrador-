281e5aeb9a3041961bb114fe519df2cf7ee1be25
remier275 <remier2745@gmail.com>
2023-08-30 19:29:22 -0300

    adios

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const app = express()
 const mainRutas = require('./routes/main.js')
+const methodOverride = require("method-override")
+app.use(methodOverride('_method'))
+
 app.use(express.static('public'))
 
 app.listen(5000,() => (console.log("servidor abierto")))
@@ -8,3 +11,6 @@ app.listen(5000,() => (console.log("servidor abierto")))
 app.set('view engine','ejs')
 
 app.use('/',mainRutas)
+
+app.use(express.urlencoded({ extended:false }))
+app.use(express.json())
