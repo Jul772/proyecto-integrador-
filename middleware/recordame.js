2964952c62f65a88f5270741810e64699db8c8e4
Samux33 <samuelagustin332008@gmail.com>
2023-11-12 14:55:53 -0300

    middleware recordame

--- middleware/recordame.js
+++ middleware/recordame.js
@@ -1,22 +1,13 @@
 let fs = require("fs")
 
-function recordame(req,res,next){
-    if(req.cookies.recordar != undefined && req.session.usuarioLoged == undefined){
-        let usersJSON = fs.readFileSync("users.json",{errors : errors.errors})
-        let users
-        if (usersJSON == ""){
-        users=[]
-        } else {
-        users = JSON.parse(usersJSON)
+function recordame (req,res,next) {
+    if(req.cookies.recordar != undefined && req.session.usuarioLogeado == undefined){
+        const {email,password}=req.body
+        const user = db.User.findOne({ where: { email } });
+        if (user){
+            req.session.usuarioLogeado = user;
+            ;
         }
-        let usuariologin
-        for (let i = 0; i < users.length; i++){
-            if (users[i].email == req.cookies.recordar ) {
-                let usuariologin = users[i]
-                break;
-            }
-        }
-        req.body.usuarioLoged = usuariologin
     }
     next()
 }
