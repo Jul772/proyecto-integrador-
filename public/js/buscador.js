72c142170900b674ab5f4e44c75e4a22422ce7da
remier275 <remier2745@gmail.com>
2023-12-06 19:09:47 -0300

    buscador

--- public/js/buscador.js
+++ public/js/buscador.js
@@ -1,5 +1,10 @@
 document.addEventListener("keyup",(e)=>{
     if(e.target.matches("#buscador")){
-        document.querySelectorAll(".articulo")
+        if(e.key === "Escape")e.target.value = ""
+        document.querySelectorAll(".articulo").forEach(element => {
+            element.textContent.toLowerCase().includes(e.target.value.toLowerCase())
+            ?element.classList.remove("filtro")
+            :element.classList.add("filtro")
+        })
     }
 })
